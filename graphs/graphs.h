a6fc1838366a93d6dc7f5dd687db9f6425ec33cf
devark28 <e.shimwa1@alustudent.com>
2026-05-24 20:02:18 +0200

    task 0

--- graphs/graphs.h
+++ graphs/graphs.h
@@ -1,4 +1,20 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct graph_s - Representation of a graph
+ * We use an adjacency linked list to represent our graph
+ * @nb_vertices: Number of vertices in our graph
+ * @vertices: Pointer to the head node of our adjacency linked list
+ */
+typedef struct graph_s
+{
+	size_t nb_vertices;
+	vertex_t *vertices;
+} graph_t;
+graph_t *graph_create(void);
 #endif
