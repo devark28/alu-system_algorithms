952e46e1344465817806bd138b009dc54c5804b2
devark28 <e.shimwa1@alustudent.com>
2026-05-24 20:29:28 +0200

    task 1

--- graphs/graphs.h
+++ graphs/graphs.h
@@ -5,6 +5,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct vertex_s vertex_t;
+
+/**
+ * struct vertex_s - Node in the linked list of vertices in the adjacency list
+ * @index: Index of the vertex in the adjacency list.
+ * @content: Custom data stored in the vertex (here, a string)
+ * @nb_edges: Number of connections with other vertices in the graph
+ * @edges: Pointer to the head node of the linked list of edges
+ * @next: Pointer to the next vertex in the adjacency linked list
+ *   This pointer points to another vertex in the graph, but it
+ *   doesn't stand for an edge between the two vertices
+ */
+struct vertex_s
+{
+	size_t index;
+	char *content;
+	size_t nb_edges;
+	edge_t *edges;
+	struct vertex_s *next;
+};
+
 /**
  * struct graph_s - Representation of a graph
  * We use an adjacency linked list to represent our graph
@@ -17,4 +38,12 @@ typedef struct graph_s
 	vertex_t *vertices;
 } graph_t;
 graph_t *graph_create(void);
+vertex_t *graph_add_vertex(graph_t *graph, const char *str);
+
+int graph_add_edge(graph_t *graph, const char *src, const char *dest, edge_type_t type);
+
+void graph_delete(graph_t *graph);
+size_t depth_first_traverse(const graph_t *graph, void (*action)(const vertex_t *v, size_t depth));
+size_t breadth_first_traverse(const graph_t *graph, void (*action)(const vertex_t *v, size_t depth));
+
 #endif
